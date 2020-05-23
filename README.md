a6d8e388c599f30134bc784bf30a7ec48b3b45d1
Tiffany Zhang <zhangtiff@google.com>
2020-05-22 23:30:11 -0700

    Set up basic React project.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# flowers
-Simple Animal Crossing New Horizons flower layout tool
+Animal Crossing Flower Planner
+---
+
+A tiny React app to help you plan flower layouts in Animal Crossing: New Horizons
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <button
+      onClick={() => {
+        setCount(count + 1);
+      }}
+    >
+      Flowers: {count}
+    </button>
+  );
+}
+
+export default App;
