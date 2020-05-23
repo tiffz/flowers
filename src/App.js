c40a403bf18e6aed5dd49967a232002208d1b1c8
Tiffany Zhang <zhangtiff@google.com>
2020-05-22 23:56:56 -0700

    Added flower images.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React, {useState} from 'react';
 
+// Flower IDs start from 0 and are monotonically increasing up to this number.
+const HIGHEST_FLOWER_ID = 53;
+
 function App() {
   const [count, setCount] = useState(0);
 
@@ -10,6 +13,7 @@ function App() {
       }}
     >
       Flowers: {count}
+      <img src={`images/flowers/${count}.png`} />
     </button>
   );
 }
